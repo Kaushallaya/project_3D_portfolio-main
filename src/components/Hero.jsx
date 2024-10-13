dcd200a4f41f9a4ee8130b6fac4aa9a1cb055e08
Kaushallaya <57025201+Kaushallaya@users.noreply.github.com>
2024-10-13 09:06:49 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi,I am <span className='text-[#915EFF]'>Bagya</span>
+            Hi,I'm <span className='text-[#915EFF]'>Bagya</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             a Frontend and backend <br className='sm:block hidden' />
